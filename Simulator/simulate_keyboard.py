641a71333f365db10ed3bb47c15749fe94335b9f
Chara Dust <81806862+CharaDust@users.noreply.github.com>
2025-05-10 21:03:57 +0800

    添加第三方平台支持

--- Simulator/simulate_keyboard.py
+++ Simulator/simulate_keyboard.py
@@ -1,5 +1,6 @@
 from pynput.keyboard import Key, Controller
 from time import sleep
+import random
 
 def Type(path: str = None, delay: int = None, code: str = None, key_delay: float = 0.1, line_delay: float = 0.1):
     # If the Argument is negative
@@ -30,8 +31,99 @@ def Type(path: str = None, delay: int = None, code: str = None, key_delay: float
     keyboard = Controller()
     for line in code.split("\n"):
         for char in line:
-            keyboard.type(char)
-            sleep(key_delay)  # Add a key_delay seconds between each keystroke
+            # keyboard.type(char)
+            if char.isupper():
+                keyboard.press(Key.shift)
+                keyboard.press(char.lower())
+                keyboard.release(char.lower())
+                keyboard.release(Key.shift)
+            elif char == '!':
+                keyboard.press(Key.shift)
+                keyboard.press(key='1')
+                keyboard.release(key='1')
+                keyboard.release(Key.shift)
+            elif char == '@':
+                keyboard.press(Key.shift)
+                keyboard.press(key='2')
+                keyboard.release(key='2')
+                keyboard.release(Key.shift)
+            elif char == '#':
+                keyboard.press(Key.shift)
+                keyboard.press(key='3')
+                keyboard.release(key='3')
+                keyboard.release(Key.shift)
+            elif char == '$':
+                keyboard.press(Key.shift)
+                keyboard.press(key='4')
+                keyboard.release(key='4')
+                keyboard.release(Key.shift)
+            elif char == '%':
+                keyboard.press(Key.shift)
+                keyboard.press(key='5')
+                keyboard.release(key='5')
+                keyboard.release(Key.shift)
+            elif char == '^':
+                keyboard.press(Key.shift)
+                keyboard.press(key='6')
+                keyboard.release(key='6')
+                keyboard.release(Key.shift)
+            elif char == '&':
+                keyboard.press(Key.shift)
+                keyboard.press(key='7')
+                keyboard.release(key='7')
+                keyboard.release(Key.shift)
+            elif char == '*':
+                keyboard.press(Key.shift)
+                keyboard.press(key='8')
+                keyboard.release(key='8')
+                keyboard.release(Key.shift)
+            elif char == '(':
+                keyboard.press(Key.shift)
+                keyboard.press(key='9')
+                keyboard.release(key='9')
+                keyboard.release(Key.shift)
+            elif char == ')':
+                keyboard.press(Key.shift)
+                keyboard.press(key='0')
+                keyboard.release(key='0')
+                keyboard.release(Key.shift)
+            elif char == '+':
+                keyboard.press(Key.shift)
+                keyboard.press(key='=')
+                keyboard.release(key='=')
+                keyboard.release(Key.shift)
+            elif char == ':':
+                keyboard.press(Key.shift)
+                keyboard.press(key=';')
+                keyboard.release(key=';')
+                keyboard.release(Key.shift)
+            elif char == '?':
+                keyboard.press(Key.shift)
+                keyboard.press(key='/')
+                keyboard.release(key='/')
+                keyboard.release(Key.shift)
+            elif char == '<':
+                keyboard.press(Key.shift)
+                keyboard.press(key=',')
+                keyboard.release(key=',')
+                keyboard.release(Key.shift)
+            elif char == '>':
+                keyboard.press(Key.shift)
+                keyboard.press(key='.')
+                keyboard.release(key='.')
+                keyboard.release(Key.shift)
+            else:
+                keyboard.press(char)  # 直接输入字符
+                keyboard.release(char)
+            # add a random time delay
+            # min = raw_delay * 0.1
+            # max = raw_delay * 1.9
+            rdm_amplifier = 0.9
+            key_delay_simui_max = int((key_delay*(1+rdm_amplifier))*1000)
+            key_delay_simui_min = int((key_delay*(1-rdm_amplifier))*1000)
+            key_delay_simui = random.randint(key_delay_simui_min,key_delay_simui_max)
+            key_delay_simuf = key_delay_simui/1000.0
+            sleep(key_delay_simuf)  # Add a key_delay seconds between each keystroke
         keyboard.tap(Key.enter)
         keyboard.tap(Key.home)
         sleep(line_delay)  # Add a line_delay seconds between each line
